90234c33ad8a2235e2e8241137bbeab364de91db
Bini Chand <binichand521@gmail.com>
2025-05-19 07:57:59 +0545

    added jsonify

--- camera_flask_app.py
+++ camera_flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, redirect, url_for, flash
+from flask import Flask, render_template, Response, request, redirect, url_for, flash, jsonify
 import cv2
 import datetime, time
 import os
